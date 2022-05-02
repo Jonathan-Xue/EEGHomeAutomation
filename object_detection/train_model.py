883110ebee3573ad06a6121e67972116c834d3c2
Jonathan Xue <jonathanxue50@gmail.com>
2022-05-02 16:25:32 -0500

    object detection

--- object_detection/train_model.py
+++ object_detection/train_model.py
@@ -6,6 +6,8 @@ from tflite_model_maker.config import ExportFormat
 from tflite_model_maker import model_spec
 from tflite_model_maker import object_detector
 
+MODEL_SPEC = 'efficientdet_lite1'
+
 # Setup
 assert tf.__version__.startswith('2')
 tf.get_logger().setLevel('ERROR')
@@ -13,16 +15,16 @@ logging.set_verbosity(logging.ERROR)
 
 # Model
 print("Training Model")
-spec = model_spec.get('efficientdet_lite1')
+spec = model_spec.get(MODEL_SPEC)
 train_data, validation_data, test_data = object_detector.DataLoader.from_csv('./detections.csv')
 model = object_detector.create(train_data, model_spec=spec, batch_size=32, train_whole_model=True, validation_data=validation_data)
 
 print("Evaluating Tensorflow Model")
-model.evaluate(test_data)
+print(model.evaluate(test_data))
 
 # Export
 print("Exporting Model")
 model.export(export_dir='.')
 
 print("Evaluating TensorflowLite Model")
-model.evaluate_tflite('model.tflite', test_data)
+print(model.evaluate_tflite(f'models/{MODEL_SPEC}.tflite', test_data))
